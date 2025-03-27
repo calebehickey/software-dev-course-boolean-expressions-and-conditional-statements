8a11444fd86baaf7cc93f1b807a2adbbf33e3996
Caleb Hickey <chickey@payitgov.com>
2025-03-26 20:57:54 -0500

    committing my work

--- boolean-expressions-and-conditional-statements.js
+++ boolean-expressions-and-conditional-statements.js
@@ -24,20 +24,33 @@ Paste the following code into your editor:
 
 */
 
+
 const readline = require('readline-sync');
 
 const hasTorch = true;
 const hasMap = false;
+const hasTent = false;
 
 console.log("You see two paths: one leads to the mountains, the other to the village.");
-const choice = readline.question("Do you go to the 'mountains' or the 'village'?");
+const choice = readline.question("Do you go to the 'mountains' or the 'village', or do you 'stay'? ");
 
 if (choice === "mountains" && hasTorch) {
   console.log("You safely navigate through the dark mountains.");
 } else if (choice === "mountains" && !hasTorch) {
   console.log("It's too dark to proceed. You decide to turn back.");
-} else if (choice === "village" || hasMap) {
+} else if (choice === "village" && hasMap) {
   console.log("You find your way to the village.");
+} else if (choice === "village" && !hasMap && hasTorch) {
+  console.log("You get lost on the way, and now it is dark. Do you 'turn back' or 'keep going?'");
+  const choice2 = readline.question("Enter 'turn back' or 'keep going': ")
+    if (choice2 === "turn back") {console.log("Game Over")}
+      else if (choice2 === "keep going" && hasTorch) {console.log("It's a difficult night, but you make it safely at dawn")}
+      else {console.log("You are hopelessly lost forever")}
+    }
+ else if (choice === "stay" && hasTent) {
+  console.log("You set up camp and enjoy the scenery.");
+} else if (choice === "stay" && !hasTent) {
+  console.log("You are not equipped to stay here and weather the elements.");
 } else {
   console.log("You get lost and wander aimlessly.");
 }
@@ -50,3 +63,5 @@ Add Customization and expand the game:
   - Use nested conditionals and logical operators to create complex outcomes.
 
 */
+
+
